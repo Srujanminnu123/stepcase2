ea94b9b7573d5c8e06b7b8f4809e32308b8d21cd
Srujanminnu123 <kk9895@srmist.edu.in>
2026-02-17 18:05:16 +0530

    second commit

---
+++ usecase1.java
@@ -0,0 +1,14 @@
+class usecase1 {
+    public static void main(String[] args) {
+        System.out.println(" ***  " + " " + "  ***  " + " " + "  ***  " + " " + " ****");
+        System.out.println("*       *" + " " + "*       *" + " " + "*       *" + " " + "**       ");
+        System.out.println("*       *" + " " + "*       *" + " " + "*       *" + " " + "**       ");
+        System.out.println("*       *" + " " + "*       *" + " " + "*       *" + " " + "**       ");
+        System.out.println("*       *" + " " + "*       *" + " " + "***  " + " " + " ***");
+        System.out.println("*       *" + " " + "*       *" + " " + "*         " + " " + "      *");
+        System.out.println("*       *" + " " + "*       *" + " " + "*         " + " " + "      *");
+        System.out.println("  ***   " + " " + " ***  " + " " + "*         " + " " + "**");
+
+    }
+
+}
